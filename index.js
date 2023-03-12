b6780f3611e7324cc4c710f88d4c1cd1a9c0f32e
Lukainit <santanazar123@icloud.com>
2023-03-12 12:26:19 +0200

    added some new

---
+++ index.js
@@ -0,0 +1,67 @@
+let users = []; 
+
+while (true) {
+  let choice = prompt("Выберите действие: регистрация или авторизация");
+
+  if (choice.toLowerCase() === "регистрация") {
+    let firstName = prompt("Введите имя:");
+    let lastName = prompt("Введите фамилию:");
+    let age = prompt("Введите возраст:");
+    let email = prompt("Введите email:");
+    let password = prompt("Введите пароль:");
+
+    let newUser = {
+      firstName: firstName,
+      lastName: lastName,
+      age: age,
+      email: email,
+      password: password
+    };
+
+    users.push(newUser);
+
+    console.log(`Новый пользователь ${firstName} ${lastName} был добавлен.`);
+
+    let continueRegistration = confirm("Продолжить регистрацию?");
+
+    if (!continueRegistration) {
+      break;
+    }
+
+  } else if (choice.toLowerCase() === "авторизация") {
+    let email = prompt("Введите email:");
+    let password = prompt("Введите пароль:");
+
+    let userFound = false;
+
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].email === email) {
+        if (users[i].password === password) {
+          console.log("Пользователь успешно авторизован.");
+          for (let key in users[i]) {
+            console.log(`${key}: ${users[i][key]}`);
+          }
+          userFound = true;
+          break;
+        } else {
+          console.log("Неверный пароль.");
+          userFound = true;
+          break;
+        }
+      }
+    }
+
+    if (!userFound) {
+      console.log("Пользователь с предоставленными данными не был найден.");
+    }
+
+    let continueAuthorization = confirm("Продолжить авторизацию?");
+
+    if (!continueAuthorization) {
+      break;
+    }
+
+  } else {
+    console.log("Некорректный ввод, попробуйте снова.");
+  }
+}
--- vite-project/main.js
+++ vite-project/main.js
@@ -1,60 +1,6 @@
-//function newCopy (mainObj) {
-//
-//    let copyArr = [];
-//
-//    for (let key in mainObj) {
-//        mainObj[key] = copyArr[key]
-//    }
-//    return copyArr ;
-//}
-//
-//const obk = {
-//    a : 1,
-//    b : 2,
-//    c : {
-//        d : 3,
-//        f : 4
-//    }
-//}
-//console.log(obk);
-//
-//const newObk = newCopy(obk);
-//
-//console.log(newObk);
+const x = 'd'
+const y = 's'
 
-function copy (mainObj) {
-    let newObj = [];
+const arr = [];
 
-    let key ;
-
-    for (key in mainObj) {
-        newObj[key] = mainObj[key]
-    }
-
-    return newObj
-}
-
-const obj = {
-    a : 10,
-    b : 2,
-    c :{
-        d : 2,
-        f : 4
-    }
-}
-
-const objFotTest = {
-    g : 3,
-}
-
-Object.assign(obj, objFotTest);
-
-//console.log(Object.assign(obj, objFotTest));
-
-const newObk = copy(obj);
-
-newObk.a = 30
-newObk.c.x = 10
-
-console.log(obj);
-console.log(newObk);
+console.log(arr)
