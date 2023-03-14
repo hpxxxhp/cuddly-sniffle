f599c590a09fcff639926c2a2ae224ba7ba0f7e0
Lukainit <santanazar123@icloud.com>
2023-03-14 20:55:37 +0200

    :)

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
-class Hero {
-    constructor (damage, hp, name) {
-       this.name = name
-       this.damage = damage;
-       this.hp = hp;  
-       console.log(this)
-    }
+class HeroTwo  {
+    constructor(name, surname, age) {
+
+        this.name = name;
+        this.surname = surname;
+        this.age = age
+
+    };
 }
-
-const warrior = new Hero(200, 100, 'nzr');
-const warrioTwo = new Hero(300, 100, 'mike');
-warrioTwo.damage -= 12 ;
-
-console.log(warrior);
-console.log(warrioTwo);
-
-
-let isRunning = true; 
-
-while (isRunning) {
-
-    const userPick = prompt("", '')
-
-    switch (userPick) {
-        case 'Q' || 'q':
-            isRunning === false;
-            break;
-        
-        default:
-            break;
-    }
-
-}
-
--- module-lvl-2/lesson-10/homework/home_work_10.js
+++ module-lvl-2/lesson-10/homework/home_work_10.js
@@ -1,36 +1,44 @@
-let userName = 'nzr',
-    userSurname = 'sht',
-    userAge = 18
+class User {
+  ID_BY_DEFAULT = Math.random().toString().slice(2, 8);
+  WALLET_BY_DEFAULR = 0; 
 
-const userDataDb = {
-  nameUser : userName,
-  surnameUser : userSurname,
-  ageUser : userAge
-}
-
-
-function getArrayFromObject (mainObjcet) {
-
-  let arr = [] ; 
-
-  let key ; 
-
-  for (key in mainObjcet) {
-    arr[key] = mainObjcet[key]
+  get #getName () {
+    return `name is ${this.name}`
   }
 
-  return arr
+  getDAta () {
+    return this.#getName ;
+  }
 
+  constructor (options) {
+    const {email, name, password, id, wallets} = options;
+
+    if (!password || !email) throw Error("u have to fill the fildes by email and password");
+
+    this.email = email; 
+    this.name = name; 
+    this.password = password; 
+    this.id = id ? id : this.ID_BY_DEFAULT ;
+    this.wallets = wallets ? wallets : this.WALLET_BY_DEFAULR;
+    this.data = [this.name];
+
+    console.log(this.getDAta);
+
+  }
+
+  getData () {
+    return [this.email, this.password];
+  }
+
+  get getEmail () {
+   return this.email;
+  }
 }
 
-const someRubishForTest = {
-  name : 'ok', 
-  nameTwp : 2
-}
+const user = new User ({
+  email : 'k@.ua',
+  password : "12"
+});
 
-const resTwo = getArrayFromObject(someRubishForTest);
-
-const res = getArrayFromObject(userDataDb);
-
-const fullRes = getArrayFromObject(...resTwo, ...res);
-
+const userEmail = user.getEmail
+console.log("[user email]", userEmail);
